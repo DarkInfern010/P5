686fd5c404d613dc10bc1bab4d492fddb86181bf
Roussel <darkinfern010@gmail.com>
2020-02-07 17:20:33 +0100

    Life OK

--- Life/sketch.js
+++ Life/sketch.js
@@ -1,73 +1,80 @@
 function make2DArray(cols, rows) {
-	var arr = new Array(cols);
-	for (var i = 0; i < arr.length; i++) {
-		arr[i] = new Array(rows);
-	}
-	return arr;
+    var arr = new Array(cols);
+    for (var i = 0; i < arr.length; i++){
+        arr[i] = new Array(rows);
+    }
+    return arr;
 }
 
 function checkLife() {
-	for (var i = 0; i < cols; i++) {
-		for (var j = 0; j < rows; j++) {
-			if (!grid[i][j].active) {
-				//Cellule morte si possède 3 voisines vivante => Vie
-				if (grid[i][j].countCell(i,j) == 3) {
-					grid[i][j].active = true;
-				} else {
-					grid[i][j].active = false;
-				}
-			} else {
-				//Doit posséder 2 ou 3 voisines en vie => Vie Sinon Mort
-				if (grid[i][j].countCell(i,j) == 2 || grid[i][j].countCell(i,j) == 3) {
-					grid[i][j].active = true;
-				} else {
-					grid[i][j].active = false;
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+            if (!grid[i][j].active) {
+                if (countCell(grid[i][j]) == 3) {
+                    grid[i][j].active = true;
+                }
+            } else {
+                if (countCell(grid[i][j]) < 2 || countCell(grid[i][j]) > 3) {
+                    grid[i][j].active = false;
+                }
+            }
+        }
+    }
+}
+  
+function countCell(cellule) {
+
+    var count = 0;
+
+	for (var xoff = -1; xoff <= 1; xoff++){
+		for (var yoff = -1; yoff <= 1; yoff++){
+			var i = cellule.i + xoff;
+			var j = cellule.j + yoff;
+			if (i > -1 && i < cols && j > -1 && j < rows) {
+				if(grid[i][j].active) {
+					count++;
 				}
 			}
 		}
 	}
-}
-
-function game() {
-	for (var i = 0; i < cols; i++) {
-		for (var j = 0; j < rows; j++) {
-			grid[i][j].changeColor();
-		}
-	}
+    return count;
 }
 
 var grid;
 var cols;
 var rows;
-var w = 20;
-
+var w = 10;
+  
 function setup() {
-	createCanvas(701,701);
-	cols = floor(width / w);
-	rows = floor(height / w);
-	grid = make2DArray(cols, rows);
+    createCanvas(701, 701);
+    cols = floor(701 / w);
+    rows = floor(701 / w);
+    grid = make2DArray(cols, rows);
 
-	for (var i = 0; i < cols; i++) {
-		for (var j = 0; j < rows; j++) {
-			grid[i][j] = new Cell(i, j, w);
-		}
-	}
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+        grid[i][j] = new Cell(i, j, w);
+        }
+    }
 
-	grid[10][10].active = true;
-	grid[10][11].active = true;
-	grid[10][12].active = true;
+    grid[10][10].active = true;
+    grid[10][11].active = true;
+    grid[10][12].active = true;
+    grid[12][10].active = true;
+    grid[12][11].active = true;
+    grid[12][12].active = true;
 }
 
 function draw() {
-	background(255);
+    background(255);
 
-	for (var i = 0; i < cols; i++) {
-		for (var j = 0; j < rows; j++) {
-			grid[i][j].show();
-		}
-	}
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < rows; j++) {
+        grid[i][j].show();
+        }
+    }
 
-	game();
-	checkLife();
-	
+    checkLife();
+
 }
+  
